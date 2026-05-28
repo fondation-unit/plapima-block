7053cd9f811044c9125ce38b0e1193e2180fcf23
ledob44 <olivier.imbert@unit.eu>
2026-05-28 17:46:12 +0200

    WIP refonte

--- src/unit-formations-feed/render.php
+++ src/unit-formations-feed/render.php
@@ -19,14 +19,14 @@ $formations = [];
 if ($sessionsAVenir->have_posts()):
 	foreach ($sessionsAVenir->posts as $session) {
 		$formationID = get_field('formation', $session->ID);
-		if (! in_array($formationID[0]->ID, $formations) && count($formations) < 4) {
+		if (! in_array($formationID[0]->ID, $formations) && count($formations) <= 3) {
 			$formations[] = $formationID[0]->ID;
 		}
 	}
 
 	if (count($formations) > 0):
 		?>
-		<div class="d-flex flex-md-row flex-column flex-wrap">
+		<div class="d-flex flex-md-row flex-column flex-wrap mt-4">
 			<?php
 			foreach ($formations as $formation) {
 				$post = get_post($formation);
@@ -38,16 +38,38 @@ if ($sessionsAVenir->have_posts()):
 
 				$size = wp_is_mobile() ? 'medium' : 'large';
 				$img = $illustration
-					? altTextForFormationImages($illustration, $size) : getBasicImage('2025/06',
+					? altTextForFormationImages($illustration, $size)
+					: getBasicImage('2025/06',
 						'img-bis-formations.png', wp_is_mobile() ? 'medium' : 'medium_large');
 
 				?>
-				<div class="formation-home-card d-flex flex-column py-4 rounded mb-4 lozad" data-background-image="<?php echo $img['src']; ?>">
-					<h3 class="rounded">
-						<a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>
-					</h3>
-					<div class="content d-flex flex-column rounded">
+				<div class="formation-home-card d-flex flex-column rounded mb-4">
+					<div class="image">
+						<img src="<?php echo $img['src']; ?>" alt="">
+					</div>
 
+					<div class="content d-flex flex-column">
+						<div class="themes d-flex flex-row">
+							<?php
+							$taxos = get_the_terms($post->ID, 'theme_formation');
+							if ($taxos) {
+								foreach ($taxos as $taxo):
+
+									$classe = get_term_meta($taxo->term_id, 'classe');
+									//						dd($classe);
+									?>
+									<div class="theme">
+										<i class="icon-<?php echo $classe[0]; ?>"></i>
+									</div>
+								<?php
+								endforeach;
+							}
+							?>
+						</div>
+						<h3>
+							<a href="<?php echo get_permalink($post->ID); ?>"><?php echo $post->post_title; ?></a>
+						</h3>
+						<hr>
 						<?php
 						if ($typeFormation['value'] === 'base'):
 							?>
